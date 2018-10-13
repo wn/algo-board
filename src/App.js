6ead05da81b5fd10f6a913b6d8e038c198d95fc4
dexterleng <dexterlengcs@gmail.com>
2018-10-14 04:09:46 +0800

    Remove size attr for graph-list

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
         component: props => <GraphNode {...props} />
       },
       GraphList: {
-        propAttrs: ['size', 'values'],
+        propAttrs: ['values'],
         component: props => <GraphList {...props} />
       },
       Hashtable: {
