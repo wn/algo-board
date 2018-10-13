aaddf869a6cf08ab6d317cb3a66adb256324dd4b
remo5000 <vigneshshankar@gmail.com>
2018-10-13 19:04:55 +0800

    Add group for Node

--- src/components/data-structures/Node.js
+++ src/components/data-structures/Node.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Shape, Circle, Text } from 'react-konva';
+import { Shape, Group, Circle, Text } from 'react-konva';
 import Konva from 'konva';
 
 export default class Node extends React.Component {
@@ -9,13 +9,14 @@ export default class Node extends React.Component {
 
   render() {
     return (
-      <Circle
-        radius={50}
-        stroke={this.state.color}
-        shadowBlur={5}
-        onClick={this.handleClick}
-        draggable
-      />
+      <Group draggable>
+        <Circle
+          radius={50}
+          stroke={this.state.color}
+          shadowBlur={5}
+          onClick={this.handleClick}
+        />
+      </Group>
     );
   }
 }
