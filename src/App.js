7069dc8f10e22082d346ab3320c4422085dafd85
wn96 <weineng.a@gmail.com>
2018-10-13 16:40:39 +0800

    Remove default header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
 
-
-import Whiteboard from './components/Whiteboard.js'
-import logo from './logo.svg';
+import Whiteboard from './components/Whiteboard.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
         <Whiteboard />
       </div>
     );
