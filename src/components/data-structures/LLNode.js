80121965e6af8aa6acdb553af1d04fc85ad9c3c3
Julius S <jluuiss@gmail.com>
2018-10-13 22:18:24 +0800

    Add text to Linked Lists

--- src/components/data-structures/LLNode.js
+++ src/components/data-structures/LLNode.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Arrow, Circle, Shape } from 'react-konva';
+import { Arrow, Circle, Group, Shape } from 'react-konva';
+
+import EditableText from './EditableText'
 
 export default class LLNode extends React.Component {
   state ={
@@ -26,27 +28,31 @@ export default class LLNode extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Shape
-          sceneFunc={(context, shape) => {
-            context.beginPath();
-            context.moveTo(10, 10);
-            context.lineTo(60, 10);
-            context.lineTo(60, 60);
-            context.lineTo(10, 60);
-            context.closePath();
-            context.moveTo(60, 10);
-            context.lineTo(110, 10);
-            context.lineTo(110, 60);
-            context.lineTo(60, 60);
-            context.closePath();
-            // (!) Konva specific method, it is very important
-            context.fillStrokeShape(shape);
-          }}
-          stroke="black"
-          strokeWidth={4}
+        <Group
           draggable
           onDragMove={this.updateLineStart}
-        />
+        >
+          <Shape
+            sceneFunc={(context, shape) => {
+              context.beginPath();
+              context.moveTo(10, 10);
+              context.lineTo(60, 10);
+              context.lineTo(60, 60);
+              context.lineTo(10, 60);
+              context.closePath();
+              context.moveTo(60, 10);
+              context.lineTo(110, 10);
+              context.lineTo(110, 60);
+              context.lineTo(60, 60);
+              context.closePath();
+              // (!) Konva specific method, it is very important
+              context.fillStrokeShape(shape);
+            }}
+            stroke="black"
+            strokeWidth={4}
+          />
+          <EditableText x={20} y={30}/>
+        </Group>
         <Arrow
           points={[this.state.shapeSourceX + 85, this.state.shapeSourceY + 35,
             this.state.lineEndX, this.state.lineEndY]}
