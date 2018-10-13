0fe0646c596dec161db61318e9c177a0f521cbe5
Vignesh <vigneshshankar@gmail.com>
2018-10-14 02:43:50 +0800

    Revert "🅱️rettier"

--- src/components/data-structures/EditableText.js
+++ src/components/data-structures/EditableText.js
@@ -1,19 +1,20 @@
-import React from "react";
-import { Text } from "react-konva";
-import Konva from "konva";
+import React from 'react';
+import { Text } from 'react-konva';
+import Konva from 'konva';
 
 export default class EditableText extends React.Component {
+
   state = {
     x: 0,
-    y: 0
+    y: 0,
   };
 
   render() {
-    const { x, y } = {
+    const {x, y} = {
       x: this.state.x,
       y: this.state.y,
-      ...this.props
-    };
+      ...this.props,
+    }
 
     return (
       <Text
@@ -22,7 +23,7 @@ export default class EditableText extends React.Component {
         text={this.props.text}
         align="center"
         verticalAlign="middle"
-        fill={"black"}
+        fill={'black'}
       />
     );
   }
