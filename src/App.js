256fac3a793f06e627c1a53a7ef8e1068e7df39e
remo5000 <vigneshshankar@gmail.com>
2018-10-13 16:46:29 +0800

    Use flexboxgrid

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
 import Sidebar from './components/Sidebar';
 import Whiteboard from './components/Whiteboard';
-import SplitPane from 'react-split-pane';
 
 class App extends React.Component {
   render() {
     return (
-      <SplitPane split="vertical" minSize={50} defaultSize={1000}>
-        <div style={{ backgroundColor: 'white', height: '100%' }}>
-        
-        </div>
-        <Sidebar/>
-      </SplitPane>
+      <Grid fluid>
+        <Row>
+          <Col xs={6}>
+            <Whiteboard />
+          </Col>
+          <Col xs={6}>
+            <Sidebar/>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
